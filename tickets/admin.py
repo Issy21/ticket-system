4641e27780da48dd81b6ce84558ceb8510710262
Issy21 <92893643+Issy21@users.noreply.github.com>
2026-07-09 10:43:26 +0100

    Add documentation attribution in admin.py
    Added attribution for Django Admin documentation.

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Ticket
-
+# Adapted from:
+# W3Schools (n.d.) Django Admin.
+# Available at: https://www.w3schools.com/django/django_admin.php
+# (Accessed: 4 June 2026).
 @admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'created_at')  # details of admiin managed tickets
